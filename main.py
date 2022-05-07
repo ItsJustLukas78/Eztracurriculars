6830c0aba7775e9ae5f14ddbd56d9858ea20dbd3
ItsJustLukas78 <93167066+ItsJustLukas78@users.noreply.github.com>
2022-05-06 23:02:42 -0700

    windows

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -7,11 +9,22 @@ from kivy.uix.widget import Widget
 
 Config.set('graphics', 'resizable', '1')
 
-class MainFloatLayout(FloatLayout):
+class BrowseWindow(Screen):
+    def start_button_click(self):
+        print("clicked out")
+
+class MainWindow(Screen):
+    def start_button_click(self):
+        print("clicked")
+
+class WindowManager(ScreenManager):
     pass
+
+kv = Builder.load_file("EztraCurricules.kv")
 
 class EztraCurriculesApp(App):
     def build(self):
-        return MainFloatLayout()
+        return kv
 
-EztraCurriculesApp().run()
+if __name__ == '__main__':
+    EztraCurriculesApp().run()
